48d766bb0800f26ecc09fa7863dda5ad7394aa72
dhanyashri b.v <dhanyashribv97@gmail.com>
2018-02-15 19:48:52 +0530

    first commit

--- Calci.java
+++ Calci.java
@@ -5,5 +5,14 @@ public class Calc{
 public static void main(String[] args)
 {
 int a,b;
+float result;
 Scanner s=new Scanner(System.in);
+	case 2:result=(a-b);
+	       System.out.println("sub of 2 num is:"+result);
+	       break;
 
+	case 4:result=(a/b);
+	       System.out.println("div of 2num is:"+result);
+	       break;
+}
+}
