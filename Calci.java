e7661175097145117a8fd545d115b7f3c117f550
Navya A <navya9721@gmail.com>
2018-02-15 19:50:13 +0530

    First commit

--- Calci.java
+++ Calci.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 public class Calc{
 public static void main(String[] args)
 {
-int a,b;
+int a,b,n;
 Scanner s=new Scanner(System.in);
+System.out.print("Enter\n 1.Addition \n2.Substration \n3.Multiplication \n4.Division \n5.Modulo operation\n");
+n=s.nextInt();
+switch(n)
+{
+	case 1:System.out.println("The result of addition is"+(a+b));
+	       break;
+	case 3:System.out.println("The result of multiplication is"+(a*b));
+	       break;
+	case 5:System.out.println("The result of modulo operation is"+(a%b));
+	       break;
+	default:System.out.println("Enter a valid operation");
+}
+sc.close();
+}
 
