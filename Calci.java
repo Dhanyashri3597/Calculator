64c2fbe3080fa6b4e9ab73f02a25d459c2e279ad
Nikeetha <nikeethaadiga@gmail.com>
2018-02-15 19:44:18 +0530

    First commit

--- Calci.java
+++ Calci.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 public class Calc{
 public static void main(String[] args)
 {
-int a,b;
+int a,b,result;
 Scanner s=new Scanner(System.in);
+System.out.println("Enter two numbers on which the operations are to be performed ");
+a=s.nextInt();
+b=nextInt();
+
 
